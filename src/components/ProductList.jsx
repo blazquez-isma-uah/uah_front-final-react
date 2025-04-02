a83cdb70293a55f7ceb7ba96198f223b4b780180
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-04-02 23:56:47 +0200

    Mostrar/ocultar productos al pulsar en nombre de categoria

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import ProductCard from './ProductCard';
 
 function ProductList({ categories, products, onAddToCart }) {
+
+    const [openCategories, setOpenCategories] = useState(
+        categories.reduce((acc, category) => ({
+            ...acc,
+            [category.id]: true
+        }), {} )
+    );
+
+    const toggleCategory = (categoryId) => {
+        setOpenCategories(prevState => ({
+            ...prevState,
+            [categoryId]: !prevState[categoryId]
+        }));
+    }
+    
   return (
-    <div className="container">
+    <div className="row">
       {categories.map(category => {
         const filteredProducts = products.filter(p => p.categoryId === category.id);
 
         return (
-          <div key={category.id} className="mb-5">
+          <div key={category.id} className="row category-container mb-5">
             <h3
               className="category-title bg-light p-2 rounded border mb-3"
-              data-category={category.id}
+              onClick={() => toggleCategory(category.id)}
+              style={{ cursor: 'pointer' }}
             >
               {category.name}
             </h3>
-            <div className="row">
+            <div className={`row category-container ${openCategories[category.id] ? 'expanded' : 'collapsed'}`}
+                id={`category-${category.id}`}>
               {filteredProducts.map(product => (
                 <ProductCard
                   key={product.id}
